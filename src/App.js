e3e0f02d9d0c52b5b8cdf4c60b916a68a00893aa
Nikhilx210 <nikhilrathee736@gmail.com>
2023-09-22 15:21:48 +0530

    to-do list front end project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const[count ,setcount]=useState("")
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState( () => {
+    const localval = localStorage.getItem("ITEMS")
+    if(localval == null) return[]
+    return JSON.parse(localval)
+  });
+  
+  useEffect(()=>{
+    localStorage.setItem("ITEMS",JSON.stringify(todos))
+  },[todos])
+
+  function handleadd(e) {
+    setcount((currcount)=>currcount+1)
+    e.preventDefault();
+    setTodos((currentstate) => {
+      return [
+        ...currentstate,
+        { id: count , title: newItem, completed: false },
+      ];
+    });
+    setNewItem("");
+    console.log(todos);
+  }
+  function toggletodo(id, completed) {
+    setTodos((currentstate) => {
+      return currentstate.map((todos) => {
+        if (todos.id === id) {
+          return {...todos, completed };
+        }
+        return todos;
+      });
+    });
+  }
+  function handledelete(id) {
+    setTodos(() => {
+      return todos.filter(todos => todos.id !== id);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleadd}>
+        <div className="mb-3">
+          <label>New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            className="form-control"
+            id="item"
+          />
+        </div>
+        <button type="submit" className="form-control">
+          ADD
+        </button>
+      </form>
+      <h1>To Do List</h1>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label >
+                <input
+                  checked={todo.completed}
+                  onChange={(e) => toggletodo(todo.id, e.target.checked)}
+                  type="checkbox"
+                />
+                <h3 className="todoblur">{todo.title}</h3>
+              </label>
+              <button onClick={() => handledelete(todo.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const TodoList = () => {
+  return (
+    <>
+    <ul>
+        {}
+    </ul>
+    </>
+  )
+}
+
+export default TodoList
